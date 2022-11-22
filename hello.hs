8859c17b36e877ee9cecbcb25ac3eb695a4e8819
Linus <95619282+linus-md@users.noreply.github.com>
2022-11-22 17:11:14 +0100

    c1

--- hello.hs
+++ hello.hs
@@ -1,6 +1,5 @@
-module Main where
-import System . Environment
+import System.Environment
 
-main :: IO ()
-main = do args <− getArgs
-    putStrLn ("Hello, " ++ args !! 0)
+main = do
+    args <- getArgs
+    putStrLn (args !! 0 ++ " + " ++ args !! 1 ++ " = " ++ show(read(args !! 0) + read(args !! 1)))
